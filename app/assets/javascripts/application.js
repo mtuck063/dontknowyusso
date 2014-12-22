0ec569d6e4802afa4698614a78bd351ad57ac9aa
mtuck063 <meneliktucker@gmail.com>
2014-12-22 03:28:40 -0500

    I learnt some after effects today :)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,12 +20,26 @@
 $(document).ready(function(){ 
 
 	start(); 
-	$("div#thebasis-container-1 img").hide();
-	$("#doyouknow").delay(2000).fadeOut(500);
-	$("div#thebasis-container-1 img").delay(2500).fadeIn(1500);
+	scaleVideo();
+
+
+
+	if(window.innerWidth > 450){
+		$("div#thebasis-container-1 img").hide();
+		$("#doyouknow").hide();
+		$("#doyouknow").fadeIn(1500);
+		$("#doyouknow").delay(1500).fadeOut(500);
+		$("div#thebasis-container-1 img").delay(6000).fadeIn(1500);
+	}else{
+		document.getElementById('bgvideo').style.display = 'none';
+		$("div#thebasis-container-1 img").hide();
+		$("#doyouknow").hide();
+		$("#doyouknow").delay(1000).fadeIn(1500);
+		$("div#thebasis-container-1 img").css({ 'top' : '10vh' });
+		$("div#thebasis-container-1 img").delay(1000).fadeIn(1500);
+	}
+
 	//$("div#thebasis-container-1 img").delay(2000).animate({opacity: [0.7, "linear"]}, 1500);
-
-
 
 });
 
@@ -33,9 +47,45 @@ $(document).ready(function(){
 
 window.onresize = function() {
 	start(); 
+	scaleVideo();
 };
 
 function start(){
 
+	if(window.innerWidth > 450){
+		$("#doyouknow").fadeOut(500);
+		document.getElementById('bgvideo').style.display = 'block';
+		$("div#thebasis-container-1 img").css({ 'top' : '2vh' });
 
+	}else{
+		$("#doyouknow").fadeIn(500);
+		$("div#thebasis-container-1 img").css({ 'top' : '10vh' });
+		document.getElementById('bgvideo').style.display = 'none';
+	}
+	
 }
+
+function scaleVideo() {
+
+	if(window.innerWidth > 480 ){
+		var windowHeight = $(window).height();
+		var windowWidth = $(window).width();
+		var nWidth = $('video#bgvideo')[0].videoWidth;
+		var nHeight = $('video#bgvideo')[0].videoHeight;
+
+		if( (windowWidth/windowHeight) > (nWidth/nHeight) ){
+
+			console.log('this should print');
+
+			if( (windowHeight / nHeight) > (windowWidth / nWidth) ){
+				var scale = (windowHeight / nHeight);
+			}else{
+				var scale = (windowWidth / nWidth);
+			}
+
+			$('video#bgvideo').height( nHeight * scale );
+			$('video#bgvideo').width( nWidth * scale );
+		}
+	}
+}
+
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Dontknowyusso
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
+    
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
