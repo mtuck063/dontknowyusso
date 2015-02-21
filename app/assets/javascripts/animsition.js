2398be116d9f9419083ccc1adb14a36325a1607f
mtuck063 <meneliktucker@gmail.com>
2015-02-21 18:40:23 -0500

    finished design for the art page

--- app/assets/javascripts/animsition.js
+++ app/assets/javascripts/animsition.js
@@ -168,6 +168,27 @@ $("#theartist-container-2.animsition").animsition({
     overlayParentElement  :   'body'
   });
 
+
+$("#shows-container-1.animsition").animsition({
+  
+    inClass               :   'fade-in-left-lg',
+    outClass              :   'fade-out-left-lg',
+    inDuration            :    1500,
+    outDuration           :    800,
+    linkElement           :   '.animsition-link', 
+    loading               :    true,
+    loadingParentElement  :   'body', //animsition wrapper element
+    loadingClass          :   'animsition-loading',
+    unSupportCss          : [ 'animation-duration',
+                              '-webkit-animation-duration',
+                              '-o-animation-duration'
+                            ],
+    overlay               :   false,
+    
+    overlayClass          :   'animsition-overlay-slide',
+    overlayParentElement  :   'body'
+  });
+
 });
 
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
 
-
+$(function(){ $(document).foundation(); });
 
 $(document).ready(function(){ 
 
